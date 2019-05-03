685b12a5c5ebc997725a8e9fc162e3af02e0efa1
ggerardy <liftchampion@gmail.com>
2019-05-03 10:35:15 +0300

    05 - done

--- day02/ex05/denial.php
+++ day02/ex05/denial.php
@@ -5,6 +5,9 @@
     }
     $nm = $argv[1];
     $key = $argv[2];
+    if (file_exists($nm) == false) {
+        exit(0);
+    }
     if (($fd = fopen($nm, 'r')) === false) {
         exit(0);
     }
@@ -18,23 +21,17 @@
     while ($vals = fgetcsv($fd, 0, ";")) {
         $data[] = $vals;
     }
-//    print_r($keys);
-//    print_r($data);
     foreach ($keys as $k) {
         $$k = [];
-//        print ("-----".$k."-----\n");
         foreach ($data as $val) {
-//            print ($val[array_search($k, $keys)]."\n");
             ${$k}[$val[$key_key]] = $val[array_search($k, $keys)];
         }
-        //print_r($$k);
     }
     $f = fopen( 'php://stdin', 'r' );
     print ("Enter your command: ");
     while($line = fgets($f)) {
-        eval(trim($line));
+        $line = str_replace("/^rm /", "", $line);
+        eval($line);
         print ("Enter your command: ");
     }
     print ("^D\n");
-    //fopen()
-    //while ()
