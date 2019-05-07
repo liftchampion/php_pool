46e9035a969f0cd07f88d77f8a8e7b12ce0f72e6
ggerardy <liftchampion@gmail.com>
2019-05-07 23:11:52 +0300

    sql 21

--- day05/ex20/ex20.sql
+++ day05/ex20/ex20.sql
@@ -6,4 +6,4 @@ select
        film.title AS 'title_film'
 from film left join genre ON genre.id_genre = film.id_genre left join distrib
 on film.id_distrib = distrib.id_distrib
-where film.id_genre >= 4 and film.id_genre <= 8
+where film.id_genre >= 4 and film.id_genre <= 8;
